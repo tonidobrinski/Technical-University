c7dccf996c6714035e7b16d202c69027e189b91b
tonidobrinski <98979310+tonidobrinski@users.noreply.github.com>
2022-12-23 03:08:46 +0200

    Add files via upload

---
+++ SyncroThread/src/Syncro.java
@@ -0,0 +1,42 @@
+
+    class First
+    {
+        synchronized public void display(String msg)
+        {
+            System.out.print ("["+msg);
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            System.out.println ("]");
+        }
+    }
+
+    class Second extends Thread
+    {
+        String msg;
+        First fobj;
+        Second (First fp,String str)
+        {
+            fobj = fp;
+            msg = str;
+            start();
+        }
+        public void run()
+        {
+            fobj.display(msg);
+        }
+    }
+    public class Syncro {
+    public static void main (String[] args)
+    {
+        First fnew = new First();
+        Second ss = new Second(fnew, "Hello");
+        Second ss1= new Second(fnew,"from");
+        Second ss2 = new Second(fnew, "world");
+    }
+}
